9859c8dd4b3720cf5e226344b9f44e1b9a85401e
Oh...Yeah!!! <614988210@qq.com>
2024-05-10 13:50:46 +0800

    解决消息堆积

--- mq-demo/consumer/src/main/java/com/itheima/consumer/listener/SpringRabbitListener.java
+++ mq-demo/consumer/src/main/java/com/itheima/consumer/listener/SpringRabbitListener.java
@@ -4,13 +4,41 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
 
+import java.time.LocalTime;
+
 @Slf4j
 @Component
 public class SpringRabbitListener {
 
+    /**
+     * 测试接受消息
+     * @param msg
+     */
     @RabbitListener(queues = "simple.queue")
     public void listenSimpleQueueMessage(String msg){
         System.out.println("消费者收到了simple.queue的消息：【" + msg + "】");
+    }
+
+    /**
+     * 参与消息堆积的测试
+     * @param msg
+     * @throws InterruptedException
+     */
+    @RabbitListener(queues = "work.queue")
+    public void listenWorkQueue1(String msg) throws InterruptedException {
+        System.out.println("消费者1接收到消息：【" + msg + "】" + LocalTime.now());
+        Thread.sleep(20);
+    }
+
+    /**
+     * 参与消息堆积的测试
+     * @param msg
+     * @throws InterruptedException
+     */
+    @RabbitListener(queues = "work.queue")
+    public void listenWorkQueue2(String msg) throws InterruptedException {
+        System.err.println("消费者2........接收到消息：【" + msg + "】" + LocalTime.now());
+        Thread.sleep(200);
     }
 
 }
--- mq-demo/publisher/src/test/java/com/itheima/publisher/amqp/SpringAmqpTest.java
+++ mq-demo/publisher/src/test/java/com/itheima/publisher/amqp/SpringAmqpTest.java
@@ -12,6 +12,9 @@ public class SpringAmqpTest {
     @Autowired
     private RabbitTemplate rabbitTemplate;
 
+    /**
+     * 测试消息发送
+     */
     @Test
     public void testSendMessage() {
 
@@ -20,6 +23,23 @@ public class SpringAmqpTest {
 
         rabbitTemplate.convertAndSend(queueName, message);
 
+    }
+
+    /**
+     * workQueue
+     * 向队列中不停发送消息，模拟消息堆积。
+     */
+    @Test
+    public void testWorkQueue() throws InterruptedException {
+        // 队列名称
+        String queueName = "simple.queue";
+        // 消息
+        String message = "hello, message_";
+        for (int i = 0; i < 50; i++) {
+            // 发送消息，每20毫秒发送一次，相当于每秒发送50条消息
+            rabbitTemplate.convertAndSend(queueName, message + i);
+            Thread.sleep(20);
+        }
     }
 
 }
