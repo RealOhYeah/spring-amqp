ab38ed691cfe64490911770e56ec001babda6fe1
Oh...Yeah!!! <614988210@qq.com>
2024-05-11 14:33:16 +0800

    基于注解生成队列和交换机

--- mq-demo/consumer/src/main/java/com/itheima/consumer/listener/SpringRabbitListener.java
+++ mq-demo/consumer/src/main/java/com/itheima/consumer/listener/SpringRabbitListener.java
@@ -1,6 +1,10 @@
 package com.itheima.consumer.listener;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.core.ExchangeTypes;
+import org.springframework.amqp.rabbit.annotation.Exchange;
+import org.springframework.amqp.rabbit.annotation.Queue;
+import org.springframework.amqp.rabbit.annotation.QueueBinding;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
 
@@ -59,13 +63,16 @@ public class SpringRabbitListener {
         System.out.println("消费者2所在的fanout.queue2接收到Fanout消息：【" + msg + "】");
     }
 
-
     /**
      * 监听direct类型的交换机
      * @param msg
      */
-    @RabbitListener(queues = "direct.queue1")
-    public void listenDirectQueue1(String msg) {
+    @RabbitListener(bindings = @QueueBinding(
+            value = @Queue(name = "direct.queue1"),
+            exchange = @Exchange(name = "hmall.direct", type = ExchangeTypes.DIRECT),
+            key = {"red", "blue"}
+    ))
+    public void listenDirectQueue1(String msg){
         System.out.println("消费者1接收到direct.queue1的消息：【" + msg + "】");
     }
 
@@ -73,8 +80,12 @@ public class SpringRabbitListener {
      * 监听direct类型的交换机
      * @param msg
      */
-    @RabbitListener(queues = "direct.queue2")
-    public void listenDirectQueue2(String msg) {
+    @RabbitListener(bindings = @QueueBinding(
+            value = @Queue(name = "direct.queue2"),
+            exchange = @Exchange(name = "hmall.direct", type = ExchangeTypes.DIRECT),
+            key = {"red", "yellow"}
+    ))
+    public void listenDirectQueue2(String msg){
         System.out.println("消费者2接收到direct.queue2的消息：【" + msg + "】");
     }
 
@@ -82,7 +93,11 @@ public class SpringRabbitListener {
      * 监听topicExchange消息
      * @param msg
      */
-    @RabbitListener(queues = "topic.queue1")
+    @RabbitListener(bindings = @QueueBinding(
+            value = @Queue(name = "topic.queue1"),
+            exchange = @Exchange(name = "hmall.topic", type = ExchangeTypes.TOPIC),
+            key = "china.#"
+    ))
     public void listenTopicQueue1(String msg){
         System.out.println("消费者1接收到topic.queue1的消息：【" + msg + "】");
     }
@@ -91,11 +106,14 @@ public class SpringRabbitListener {
      * 监听topicExchange消息
      * @param msg
      */
-    @RabbitListener(queues = "topic.queue2")
+    @RabbitListener(bindings = @QueueBinding(
+            value = @Queue(name = "topic.queue2"),
+            exchange = @Exchange(name = "hmall.topic", type = ExchangeTypes.TOPIC),
+            key = "#.news"
+    ))
     public void listenTopicQueue2(String msg){
         System.out.println("消费者2接收到topic.queue2的消息：【" + msg + "】");
     }
-
 
 
 
